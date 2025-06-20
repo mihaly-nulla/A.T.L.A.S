0c10f4fd18e6796302eb079aa0a9901adb2df2eb
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2025-06-19 23:30:39 -0300

    ✨ feat: Início do desenvolvimento do Móduo de memória

---
+++ MemoryModule/components/SemanticMemory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MemoryModule.components
+{
+    public class SemanticMemory
+    {
+        private string conceptId;
+        private List<string> relatedConcepts;
+        private DateTime discoveryDate;
+
+        public SemanticMemory(string conceptId, 
+                                List<string> relatedConcepts,
+                                DateTime discoveryDate)
+        {
+            this.conceptId = conceptId;
+            this.relatedConcepts = relatedConcepts;
+            this.discoveryDate = discoveryDate;
+        }
+
+        public void AddRelatedConcept(string relatedConcept)
+        {
+            if (!relatedConcepts.Contains(relatedConcept))
+            {
+                relatedConcepts.Add(relatedConcept);
+            }
+        }
+
+        public List<string> GetRelatedConcepts()
+        {
+            return relatedConcepts;
+        }
+
+        public string GetConceptId()
+        {
+            return conceptId;
+        }
+    }
+}
---
+++ MemoryModule/systems/MemorySystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MemoryModule.systems
+{
+	public class MemorySystem
+	{
+		private 
+		public MemorySystem()
+		{
+
+		}
+	}
+}
